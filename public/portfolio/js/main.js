aeaf39cd036bda4c29774c58d6f682af2855e9e7
Dimitar "mitko8009" Mitev <mitko8009@users.noreply.github.com>
2024-05-22 02:27:54 +0300

    Projects Window in portfolio

--- public/portfolio/js/main.js
+++ public/portfolio/js/main.js
@@ -1,21 +1,42 @@
+function navBtnEffect(add = false) {
+    const GAP = 26;
+
+    $(".navbar-content").animate({backgroundColor: "rgb(110, 110, 110)"}, 20).delay(200).animate({backgroundColor: "rgb(40, 42, 43)"}, 20);
+
+    add ? 
+    $(".navbar-content").animate({width: $(".navbar-content").width() + $(".nav-link").eq(1).width() + GAP}, 300):
+    $(".navbar-content").animate({width: $(".navbar-content").width() - $(".nav-link").eq(1).width() - GAP}, 300);
+}
+
+function setOnFocus(elementId) {
+    for (let i = 0; i < $(".window").length; i++) {
+        $(".window").eq(i).css("z-index", 7 - i);
+    }
+
+    $("#" + elementId).css("z-index", 8);
+}
+
+function closeWindow(elementId) {
+    navBtnEffect(true);
+
+    $("#win_" + elementId).animate({opacity: 0}, 300, () => {
+        $("#win_" + elementId).hide();
+        $("#nav_" + elementId).css("opacity", 0).show().animate({opacity: 1}, 50);
+    });
+}
+
+function openWindow(elementId) {
+    navBtnEffect(false);
+    setOnFocus("win_" + elementId);
+
+    $("#nav_" + elementId).animate({opacity: 0}, 200, () => {
+        $("#nav_" + elementId).hide();
+    });
+
+    $("#win_" + elementId).css({opacity: 0}).show().animate({opacity: 1}, 200);
+}
+
 $(document).ready(() => {
-    var zIndexOnTop = 7;
-
-    function navBtnEffect(add = false) {
-        const GAP = 24;
-
-        $(".navbar-content").animate({backgroundColor: "rgb(110, 110, 110)"}, 20).delay(200).animate({backgroundColor: "rgb(40, 42, 43)"}, 20);
-        add ? $(".navbar-content").animate({width: $(".navbar-content").width() + $(".nav-link").eq(1).width() + GAP}) : $(".navbar-content").animate({width: $(".navbar-content").width() - $(".nav-link").eq(1).width() - GAP});
-    }
-
-    function setOnFocus(elementId) {
-        for (let i = 0; i < $(".window").length; i++) {
-            $(".window").eq(i).css("z-index", 7 - i);
-        }
-
-        $("#" + elementId).css("z-index", 8);
-    }
-
     $(".draggable").draggable({
         handle: ".winHeader",
         containment: ".content",
@@ -26,72 +47,25 @@ $(document).ready(() => {
         stack: ".window",
     }).resizable();
 
-    // About
-    $("#closeWindowBtn").click(() => {
-        navBtnEffect(true);
-
-        $("#pjson").animate({opacity: 0}, 300, () => {
-            $("#pjson").hide();
-            $("#portfolioHome").css("opacity", 0).show().animate({opacity: 1}, 50);
-        });
-    });
-
-    $("#portfolioHome").click(() => {
-        navBtnEffect(false);
-        setOnFocus("pjson");
-
-        $("#portfolioHome").animate({opacity: 0}, 200, () => {
-            $("#portfolioHome").hide();
-        });
-
-        $("#pjson").css({opacity: 0}).show().animate({opacity: 1}, 200);
-    });
+    // Portfolio
+    $("#win_portfolio_close").click(() => { closeWindow("portfolio"); });
+    $("#nav_portfolio").click(() => { openWindow("portfolio"); });
 
     // Projects
-    $("#closeWinProjectsBtn").click(() => {
-        navBtnEffect(true);
-
-        $("#winProjects").animate({opacity: 0}, 300, () => {
-            $("#winProjects").hide();
-            $("#projectsIcon").css("opacity", 0).show().animate({opacity: 1}, 50);
-        });
-    });
-
-    $("#projectsIcon").click(() => {
-        navBtnEffect(false);
-        setOnFocus("winProjects");
-
-        $("#projectsIcon").animate({opacity: 0}, 200, () => {
-            $("#projectsIcon").hide();
-        });
-        
-        $("#winProjects").css({opacity: 0}).show().animate({opacity: 1}, 200);
-    });
+    $("#win_projects_close").click(() => { closeWindow("projects"); });
+    $("#nav_projects").click(() => { openWindow("projects"); });
 
     // Awards
-    $("#closeWinAwardsBtn").click(() => {
-        navBtnEffect(true);
+    $("#win_awards_close").click(() => { closeWindow("awards"); });
+    $("#nav_awards").click(() => { openWindow("awards"); });
 
-        $("#winAwards").animate({opacity: 0}, 300, () => {
-            $("#winAwards").hide();
-            $("#awardsIcon").css("opacity", 0).show().animate({opacity: 1}, 50);
-        });
-    });
-
-    $("#awardsIcon").click(() => {
-        navBtnEffect(false);
-        setOnFocus("winAwards");
-
-        $("#awardsIcon").animate({opacity: 0}, 200, () => {
-            $("#awardsIcon").hide();
-        });
-
-        $("#winAwards").css({opacity: 0}).show().animate({opacity: 1}, 200);
-    });
+    // Shkolo Tweaks
+    $("#win_ShTw_close").click(() => { closeWindow("ShTw"); });
+    $("#nav_ShTw").click(() => { openWindow("ShTw"); });
     
 
     $(".nav-link").tooltip({
         tooltipClass: "navbar-tooltip",
-        position: {my: "center top+13", at: "center bottom"},
+        position: {my: "center top+14", at: "center bottom"},
     });
 });
