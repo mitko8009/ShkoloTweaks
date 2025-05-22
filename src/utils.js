7f7c70f31367b6edc099bde2becd2cd4e6dc2492
mitko8009 <dimitarvlm@gmail.com>
2025-05-22 23:59:10 +0300

    style: update dark theme colors

--- src/utils.js
+++ src/utils.js
@@ -46,6 +46,6 @@ function isColorLight(hex) {
     const r = parseInt(hex.substr(0,2),16)
     const g = parseInt(hex.substr(2,2),16)
     const b = parseInt(hex.substr(4,2),16)
-    // Perceived brightness formula
+
     return (r*0.299 + g*0.587 + b*0.114) > 186
 }
